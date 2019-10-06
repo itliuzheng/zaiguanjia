965dc33054642bb05d59f975f177457ffb6d00cb
itliuzheng <itliuzheng@sina.com>
2019-10-06 19:15:47 +0800

    index

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -22,31 +22,40 @@
           <img class="company-banner" src="@/images/about/banner.jpg" alt="">
           <div class="company-desc">
 
-            <p>以下为测试文字 </p>
-            <p>依托于互联网的信息交互，充分的整合市场上丰富的资产资源与处置服务机构，优化资产处置业务流程，有效降低资产处置过程中的沟通成本与处置成本，从而显著提高处置回收率与时效性。  </p>
-            <p>We've been crafting beautiful websites, launching stunning brands and making clients happy for years.
-Phasellus enim libero, blandit vel sapien vitae, condimentum ultricies magna et. Quisque euismod lobortis aliquam. Aliquam in tortor enim.
-orci ut et lobortis aliquam. Aliquam in tortor enim.</p>
-            <p>资产处置的主要方式有：调拨、变卖、报损、报废以及将非经营性资产转为经营性资产（以下简称“非转经”）等。资产处置应逐级申报，分级审批：（一）一次性处置价值原值20万元以下的资产，由各部门审批，报国管局备案。（二）一次性处置价值原值20万元（含）以上的资产，需经资产使用单位提出申请，报国管局审批。（三）国有土地、房屋及建筑物、汽车的处置，均报国管局审批。</p>
-            <p>资产处置的主要方式有：调拨、变卖、报损、报废以及将非经营性资产转为经营性资产（以下简称“非转经”）等。资产处置应逐级申报，分级20万元（含）以上的资产，需经资产使用单位提出申请，报国管局审批。（三）国有土地、房屋及建筑物、汽车的处置，均报国管局审批。</p>
-            <p>资产处置的主要方式有：调拨、变卖、报损、报废以及将非经营性资产转为经营性资产（以下简称“非转经”）等。资产处置应逐级申报，分</p>
-            <p>资产处置的主要方式有：调拨、变卖、报损、报废以及将非经营性资产转为经营性资产（以下简称“非转经”）等。资产处置应逐级申报，分级审批：（一）一次性处置价值原值20万元以下的资产，由各部门审批，报国管局备案。（二）一次性处置价值原值20万元（含）以上的资产，需经资产使用单位提出申请，报国管局审批。（三）国有土地、房屋及建筑物、汽车的处置，均报国管局审批。</p>
+            <p>公司简介：</p>
+            <p>北京债管家科技有限公司旗下的一站式债权管 理服务平台——债管家，是基于大数据及区块链技术， 通过债权登记、证据链保全、智能催收、债权委托催收及转让交易，提供一站式贷后管理服务专业平台。 平台同时通过精准的债务人大数据查询及信用分析系 统，提供贷前风控决策分析服务。</p>
+            <p>债管家团队由线上大数据及区块链行业专家和 线下法律及专业催收队伍组成，拥有完整的线上线下 结合的团队结构和行业能力。</p>
+<p>债管家自身的大数据体系已经在旅游贷、消费贷、 中小企业金融服务领域进行了多年的实践，有比较大的数据资源和被实践检验的风控模型。同时债管家在全国重要省份都有专业的本地合作团队，可以实现本 地化服务。</p>
+<p>债管家旨在帮助民间借贷中，债权人及相关机构 轻松管理债权，并在合法合规情况下，实现对债权的 有效催收，构建良性的民间借贷生态体系，实现民间借贷信用大数据化、管理催收智能化、债权交易生态化。</p>
+
           </div>
         </div>
 
       </div>
       <div class="company" v-if="activeName == 'business'">
 
-        <div class="about-header">
+        <div class="about-header about-header-business">
         </div>
         <div class="company-content">
           <h3 class="company-title">
             商务合作
           </h3>
-          <p class="english-desc">DEBT STEWARD'S NON-PERFORMING ASSETS DISPOSAL PLATFORM</p>
-          <img class="company-banner" src="@/images/about/banner.jpg" alt="">
-          <div class="company-desc">
-            <p>以下为测试文字 </p>
+          <p class="english-desc">CONTACT US</p>
+          <div class="contact-info clearfix">
+            <div class="icon fl"><img src="@/images/about/contact/hezuo.jpg" alt=""></div>
+            <div class="info-desc fl">
+              <ul class="info-ul">
+                <li>商务合作共创未来，欢迎拨打合作电话</li>
+                <li>联系电话：+86 10 69887650</li>
+                <li>电子邮箱：website@webs.com</li>
+                <li>公司地址：北京市 朝阳区 建外三百六十二号4层</li>
+                <li>邮政编码：100024</li>
+              </ul>
+            </div>
+          </div>
+          <div class="company-contact-desc">
+            <p class="chinese">债管家秉承共同发展的理念，鼓励客户一起共谋互联网民间借贷的未来，共同努力，共享发展成功————这是我们共同的债管家</p>
+            <p class="english"></p>
           </div>
         </div>
 
@@ -181,6 +190,10 @@ orci ut et lobortis aliquam. Aliquam in tortor enim.</p>
       transform: translate(-50%, 0);
       -webkit-background-size: auto 100%;
       background-size: auto 100%;
+    }
+    .about-header-business{
+      background: url("~@/images/about/contact/beijing.jpg") no-repeat center;
+      height: 786px;
     }
     .about-header-contact{
       background: url("~@/images/about/contact/bg.jpg") no-repeat center;
